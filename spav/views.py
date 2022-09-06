d7477dfa78f3a2bf9b53a9e9a9b5f6e8695f87dd
Ingenious <ingeniousabhinav@gmail.com>
2022-09-06 17:12:52 +0530

    Added overlay effect and many other things

--- spav/views.py
+++ spav/views.py
@@ -1,22 +1,35 @@
 from multiprocessing import context
 from django.shortcuts import render
 from .models import Alert, Noticeboard, UpcomingLect
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 lectureData = UpcomingLect.objects.all()
-noticetData = Noticeboard.objects.all()
+noticeData = Noticeboard.objects.all()
 alertData = Alert.objects.all()
+
+# noticeboard pagination #
+
+
+# noticeboard pagination #
 
 context = {
     'alert': alertData,
-    'notice': noticetData,
+    'notice':noticeData,
     'lecture': lectureData,
 }
-
 
 def index(request):
     return render(request, 'index.html',context)
 
 def noticeboard(request):
-    return render(request, 'noticeboard.html',context)
+    noticeData = Noticeboard.objects.all()
+    paginator = Paginator(noticeData, 2) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    contextt = {
+        'notice': noticeData,
+        'page':page_obj
+    }
+    return render(request, 'noticeboard.html',contextt)
