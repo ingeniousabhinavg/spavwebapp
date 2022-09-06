46d05fd3434d6d6720d8d4cb18749d04c15fa43f
Ingenious <ingeniousabhinav@gmail.com>
2022-09-06 12:17:58 +0530

    Added Alert and Noticeboard Model

--- spav/models.py
+++ spav/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Alert(models.Model):
+    alertTitle = models.CharField(max_length=100)
+    alertFile = models.FileField(upload_to='alert', max_length=100)
+    alertDate = models.DateField(auto_now=False, auto_now_add=False)
+    ALERT_CHOICES = (
+        ('danger', 'danger'),
+        ('warning','warning'),
+        ('primary','primary'),
+        ('light','light'),
+    )
+    alertIntensity = models.CharField( max_length=50, choices=ALERT_CHOICES)
+
+    def __str__(self) -> str:
+        return self.alertTitle
+
+class Noticeboard(models.Model):
+    noticeTitle = models.CharField(max_length=200)
+    noticeFile = models.FileField(upload_to='noticeboard', max_length=100)
+    noticeUrl = models.URLField(max_length=200, null=True)
+    noticeDate = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self) -> str:
+        return self.noticeTitle
--- spav/views.py
+++ spav/views.py
@@ -1,6 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Alert
 
 # Create your views here.
 
+alertData = Alert.objects.all
+
+context = {
+    'alert': alertData
+}
+
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html',context)
